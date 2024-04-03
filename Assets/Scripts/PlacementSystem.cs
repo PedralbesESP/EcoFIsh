86e8e7a604b63f7e146ce3edf1fb3ace44c72743
Zoe Guerra Parra <a23zoeguepar@inspedralbes.cat>
2024-04-03 20:54:23 +0200

    probando cosas

---
+++ Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator;
+    [SerializeField]
+    private InputManager inputManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        mouseIndicator.transform.position = new Vector3(mousePosition.x, mousePosition.y, -1);
+    }
+}
